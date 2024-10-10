187b79a8296ad5f6d3c73d6551ef873dba99e1a8
Alan Alves <alan.alveses@gmail.com>
2024-10-10 19:15:29 -0300

    first commit

---
+++ src/modules/personagem.js
@@ -0,0 +1,23 @@
+export class Personagem {
+    nome
+    vida 
+    mana
+    level
+    tipo
+    descricao
+
+    constructor(nome, level, tipo){
+        this.nome = nome
+        this.level = level
+        this.tipo = tipo
+        this.vida = 100
+        this.mana = 100
+    }
+
+    obterInsignia() {
+        if (this.level >= 5){
+            return `${this.tipo} Implacavel`
+        } 
+        return `${this.tipo} iniciante.`
+    }
+}
