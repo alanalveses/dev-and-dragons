c2bc15b94082a827a3e7508de43b3b8991eb324b
Alan Alves <alan.alveses@gmail.com>
2024-11-24 14:17:12 -0300

    final commit

--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -1,23 +1,51 @@
 export class Personagem {
     nome
-    vida 
-    mana
-    level
+    vida = 100
+    mana = 100
+    #level
     tipo
     descricao
 
-    constructor(nome, level, tipo){
+    constructor(nome) {
         this.nome = nome
-        this.level = level
-        this.tipo = tipo
-        this.vida = 100
-        this.mana = 100
+        this.#level = 1
+    }
+
+    aumentarLevel() {
+        this.level += 1
+    }
+
+    diminuirLevel() {
+        this.level -= 1
+    }
+
+    get level() {
+        return this.#level
+    }
+
+    set level(novoLevel) {
+        if (novoLevel >= 1 && novoLevel <= 10) {
+            this.#level = novoLevel
+        }
     }
 
     obterInsignia() {
-        if (this.level >= 5){
-            return `${this.tipo} Implacavel`
-        } 
-        return `${this.tipo} iniciante.`
+        if (this.#level >= 5) {
+            return `Implacavel ${this.constructor.tipo}`
+        }
+        return `${this.constructor.tipo} iniciante`
     }
+
+    static verificarVencedor(personagem1, personagem2) {
+        if (personagem1.level === personagem2.level) {
+            return 'Empate!!!'
+        }
+
+        if (personagem1.level > personagem2.level) {
+            return `${ personagem1.constructor.tipo } ${personagem1.nome} é o vencedor!`
+        }
+
+        return `${ personagem2.constructor.tipo } ${personagem2.nome} é o vencedor!`
+    }
+
 }
